e6f251729015cd0d4c128f231bb4ebdfcb9a7982
younghearts <331912665@qq.com>
2020-03-23 11:06:47 +0800

    新增重置密码

--- user.js
+++ user.js
@@ -40,4 +40,23 @@ router.post('/login', (req, res) => {
     return res.json(new Result({code:403,msg:'请完善账号和密码'}))
   }
 });
+//修改密码
+router.post('/change_password',(req,res)=>{
+    let {account,password}=req.body||{};
+    if(account&&password){
+      let update_user='update user set ? where account=?';
+      pool.getConnection((err,conn)=>{
+        conn.query(update_user, [{ password}, account],(e,r)=>{
+          if(e){
+            return res.json(new Result({code:e.errno,msg:e.sqlMessage}));
+          }else{
+            return res.json(r.affectedRows>0?new Result({msg:'修改成功'}):new Result({code:0,msg:'没有此账号'}));
+          }
+        })
+      })
+    }else{
+      return res.json(new Result({code:403,msg:'请完善账号和密码'}))
+    }
+})
+
 module.exports = router;
